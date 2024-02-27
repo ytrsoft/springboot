fcdbb2acffd2cd43e734874a36193b99b264bd76
ytrsoft <1654486219@qq.com>
2024-02-28 06:35:19 +0800

    add rest aspect

--- src/main/java/com/ytrsoft/aspect/RestAspect.java
+++ src/main/java/com/ytrsoft/aspect/RestAspect.java
@@ -6,22 +6,36 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Slf4j
 @Aspect
 @Component
 public class RestAspect {
 
-    @Around("execution(public com.ytrsoft.response.ResponseEntity *(..)))")
+    @Pointcut("execution(public com.ytrsoft.response.ResponseEntity *(..))")
+    public void responseEntityMethods() {}
+
+    @Pointcut("within(@org.springframework.web.bind.annotation.RestController *) || " +
+            "within(@org.springframework.data.rest.webmvc.RepositoryRestController *) || " +
+            "within(@org.springframework.stereotype.Controller *)")
+    public void controllerMethods() {}
+
+    @Around("responseEntityMethods() || controllerMethods()")
     public Object handleAspectMethod(ProceedingJoinPoint pjp) {
         long startTime = System.currentTimeMillis();
+        log.info("Starting method: {} with arguments: {}", pjp.getSignature(), Arrays.toString(pjp.getArgs()));
         try {
             Object ret = pjp.proceed();
             long elapsedTime = System.currentTimeMillis() - startTime;
-            log.info("[{}]use time: {}", pjp.getSignature(), elapsedTime);
+            log.info("Method [{}] executed successfully in [{}] ms with result: {}", pjp.getSignature(), elapsedTime, ret);
             return ret;
         } catch (Throwable e) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            log.error("Exception in method [{}] after [{}] ms. Exception: {}", pjp.getSignature(), elapsedTime, e.getMessage(), e);
             return ResponseEntity.failed(ResponseStatus.FAILED.code(), e.getMessage());
         }
     }
